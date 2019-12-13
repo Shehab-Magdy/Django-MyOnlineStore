644b8460f1d2752aaedc999d58d4a196d8b3c4f2
Shehab Magdy El-Adl <cegres1@hotmail.com>
2019-12-14 01:11:30 +0200

    adding filter to the home page

--- center/views/home.py
+++ center/views/home.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404 
 from center.models import Category, Product
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def Index(request):
+	categories = Category.objects.all()
+
 	if request.GET.get('catid'):
 		cat_id = request.GET.get('catid')
+		cat_id = int(cat_id[0])
+		if cat_id != 0:
+			if request.GET.get('lt'):
+				lt = int(request.GET.get('lt'))
+				gt = int(request.GET.get('gt'))
+				prod = Product.objects.filter(Q(price__lt=lt) & Q(price__gte=gt) &Q(category_id = cat_id))
+			else:
+				prod = Product.objects.filter(category_id = cat_id)
+		else:
+			if request.GET.get('lt'):
+				lt = int(request.GET.get('lt'))
+				gt = int(request.GET.get('gt'))
+				prod = Product.objects.filter(Q(price__lt=lt) & Q(price__gte=gt))
 	else:
-		cat_id = -1
-
-	categories = Category.objects.all()
-	if cat_id == -1:
+		cat_id = 0
 		prod = Product.objects.all()
-		
-	else:
-		prod = Product.objects.filter(category_id = cat_id)
-		
+
 	paging = Paginator(prod,6)
 	page = request.GET.get('page')
 	products = paging.get_page(page)
-	cat_name = int(cat_id)
-	data = {'cats':categories, 'prods': products, 'catName':cat_name}
+	cat_id = int(cat_id)
+	data = {'cats':categories, 'prods': products, 'catNo':cat_id}
 	return render(request,'home/index.html', data)
 
 
